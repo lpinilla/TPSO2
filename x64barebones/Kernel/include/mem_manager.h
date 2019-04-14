61d79c421fe6836fcf24e80556cb31593bde3b5f
lpinilla <lauti_nipi123@hotmail.com>
2019-04-14 17:05:57 -0300

    puesto en un solo archivo el mem_manager

---
+++ x64barebones/Kernel/include/mem_manager.h
@@ -0,0 +1,48 @@
+#ifndef _FREE_LIST_H
+#define _FREE_LIST_H
+
+#include <stddef.h>
+#include <string.h>
+
+enum STATUS {P = 0, F = 1};
+
+struct t_Node{ 
+    struct t_Node * next, *prev;
+    void * mem_ptr;
+    size_t size;
+    enum STATUS status;
+};
+
+typedef struct t_Node * Node;
+
+void * start_dir;
+
+Node root, last;
+size_t total_mem_size;
+
+
+/*función para inicializar la memoria dado un puntero inicial y el tamaño
+**total de la memoria*/
+void initialize_list(void * start_ptr, size_t total_size);
+
+/*función para agregar un nodo nuevo a la lista de alocación*/
+Node add_node(void * node_location, size_t size);
+
+/*función que implementa first fit en la lista de alocación,
+**si el espacio que encuentra es más grande que el pedido y hay espacio
+**para crear un nuevo nodo, crea un nuevo nodo que apunta a la nueva zona
+**de memoria liberada y lo agrega a la lista*/
+Node find_first_fit(Node curr, size_t size);
+
+/*función que llama a find_first_fit con el nodo raíz y 
+**retorna el puntero del nodo adecuado*/
+void * mem_alloc(size_t size);
+
+//función que llama a liberar el nodo que tenga el puntero apuntando a ptr
+void free_mem(void * ptr);
+
+//función para liberar un nodo de la lista (de ser necesario, elimilandolo) 
+Node free_node(Node curr, void * mem_ptr);
+
+#endif
+
--- x64barebones/Kernel/include/mem_manger.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MEM_MANAGER_H_
-#define _MEM_MANAGER_H_
-
-#include <stddef.h>
-
-
-void * mem_alloc(size_t size);
-
-#endif
