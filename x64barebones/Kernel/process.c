0702c11af415c8587b0e4d3ef0a842e98095d3ce
JoaquinBattilana <battilanajn@gmail.com>
2019-04-22 17:58:45 -0300

    Added process basic structure, create and delete

---
+++ x64barebones/Kernel/process.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+#define STACK_SIZE 4096
+
+typedef enum {P_START, P_READY, P_RUNNING, P_WAITING, P_TERMINATE} pstate_t;
+
+static size_t global_pid;
+
+typedef struct {
+    size_t pid;
+    pstate_t state;
+    char * stack;
+} process_t;
+
+void create_process(){
+    process_t * new_process = mem_alloc(sizeof(process_t));
+    new_process->pid = global_pid;
+    global_pid++;
+    new_process->state = P_START;
+    new_process->stack = mem_alloc(sizeof(STACK_SIZE));
+}
+
+void delete_process(process_t * process){
+    free_mem(process->stack);
+    free_mem(process);
+}
