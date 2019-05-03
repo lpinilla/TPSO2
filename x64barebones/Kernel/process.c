3baed55dec76ec01770f41ce469ab07e11e2d3f8
JoaquinBattilana <battilanajn@gmail.com>
2019-05-03 01:54:54 -0300

    Changed struct process from .c to .h

--- x64barebones/Kernel/process.c
+++ x64barebones/Kernel/process.c
@@ -5,6 +5,41 @@ static uint64_t init_stack(void * process_start, void * stack_pointer);
 
 static size_t global_pid = 0;
 
+typedef struct processADT {
+    size_t pid;
+    pstate_t state;
+    uint64_t stack_pointer;
+} processADT;
+
+typedef struct {
+	//Registers restore context
+	uint64_t gs;
+	uint64_t fs;
+	uint64_t r15;
+	uint64_t r14;
+	uint64_t r13;
+	uint64_t r12;
+	uint64_t r11;
+	uint64_t r10;
+	uint64_t r9;
+	uint64_t r8;
+	uint64_t rsi;
+	uint64_t rdi;
+	uint64_t rbp;
+	uint64_t rdx;
+	uint64_t rcx;
+	uint64_t rbx;
+	uint64_t rax;
+
+	//iretq hook
+	uint64_t rip;
+	uint64_t cs;
+	uint64_t eflags;
+	uint64_t rsp;
+	uint64_t ss;
+	uint64_t base;
+}stack_t;
+
 process_t create_process(void * process_start){
 
     process_t new_process = mem_alloc(sizeof(*new_process));
