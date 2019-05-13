96e622c2b5e448742b979e6eb1e48b656dd91b1d
Tomas Dorado <tdorado@itba.edu.ar>
2019-05-13 15:36:47 -0300

    Agregado mutex de read y de write separados

--- x64barebones/Kernel/ipc.c
+++ x64barebones/Kernel/ipc.c
@@ -3,19 +3,21 @@
 
 static Message mailbox[MAX_MESSAGES];
 static int sem;
-static int lock;
+static int read_lock;
+static int write_lock;
 
 void init_mailbox(){
     memset(mailbox, 0, MAX_MESSAGES * sizeof(Message));
     sem = my_sem_open("ipc");
-    lock = 0;
+    read_lock = 0;
+    write_lock = 0;
 }
 
 void my_write(char * msg, int id){
     int i, found = 0;
     //vemos si podemos escribir
     while(my_sem_get_value(sem) > MAX_MESSAGES);
-    lock_mutex(&lock);   //ganar el recurso
+    lock_mutex(&write_lock);   //ganar el recurso
     //encontrar el 1er espacio libre
     for(i = 0; i < MAX_MESSAGES && !found; i++){
         if( mailbox[i] == NULL ) found = 1;
@@ -25,13 +27,13 @@ void my_write(char * msg, int id){
     str_cpy(msg, aux->msg);
     mailbox[i] = aux;
     my_sem_post(sem); //sumarle al semáforo para indicar que hay un mensaje disponible
-    unlock_mutex(&lock);    //liberar el recurso
+    unlock_mutex(&write_lock);    //liberar el recurso
 }
 
 void my_read(int id, char * ret){
     int found = 0;
     my_sem_wait(sem);    //esperar al semáforo a qué haya algo para leer
-    lock_mutex(&lock);   //ganar el recurso
+    lock_mutex(&read_lock);   //ganar el recurso
     for(int i = 0; i < MAX_MESSAGES && !found; i++){
         //ver si el mensaje es para un pid que esta vivo
         if(mailbox[i]->id == id){
@@ -43,12 +45,12 @@ void my_read(int id, char * ret){
     }
     if(!found){
         my_sem_post(sem); //decrementar el semáforo número si se leyó
-        unlock_mutex(&lock);
+        unlock_mutex(&read_lock);
         _context_switch_process();
         my_read(id, ret);
         return;
     }
-    unlock_mutex(&lock);    //liberar el recurso
+    unlock_mutex(&read_lock);    //liberar el recurso
 }
 
 void * get_mailbox_address(){
